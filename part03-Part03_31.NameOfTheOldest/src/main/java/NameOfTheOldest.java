4a61af1f961246febc998bbf50a8c313057f981a
bhunk <bhunkin@live.com>
2021-09-22 17:15:27 -0400

    part 03_31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,6 +5,26 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int max = 0;
+        String name = "";
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+            for (int i = 1; i < Integer.valueOf(parts[1]); i++){
+            if (Integer.valueOf(parts[1]) > max)
+            {
+             max = Integer.valueOf(parts[1]);
+             name = parts[0];
+            }
+            
+        }
+    }
+        System.out.println("Name of the oldest: " + name);
 
 
     }
