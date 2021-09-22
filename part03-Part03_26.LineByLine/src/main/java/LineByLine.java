c0cf8a8790f916682994b50afd9431987547a352
bhunk <bhunkin@live.com>
2021-09-22 02:07:50 -0400

    part 03_26

--- part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,20 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+       // for (int i =0; i< uhh; i++){
 
-
+    while(true){
+        String text = scanner.nextLine();
+        String[] pieces = text.split(" ");
+        if(text.equals("")){
+               break;
+        }else{
+        for(int i=0;i<pieces.length;i++){
+            System.out.println(pieces[i]);
+           }
+       }
+       
+    }
     }
 }
+
