aec10dc83b95641580deb4da324995854beba3c1
bhunk <bhunkin@live.com>
2021-09-22 22:24:37 -0400

    part 03_32

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,31 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String name = "";
+        String Lname = "";
+        int sum = 0;
+        int count = 0;
 
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
 
+            String[] parts = input.split(",");
+        //longest name
+             name = parts[0];
+            if (name.length() > Lname.length()) {
+                Lname = name;
+            }
+        //average of numbers
+            for (int i = 1; i < parts.length; i++){
+                count++;
+            }
+            sum = sum + Integer.valueOf(parts[1]);
+                
+            }
+               System.out.println("Name of the oldest: " + Lname); 
+               System.out.println("Average of birth years: " +(double)sum / (double)count);
     }
 }
